f568519dc64928d91879e0a5b3605dad2bfbc915
aliyilmaz020 <aliyilmaz@ogr.bandirma.edu.tr>
2024-01-10 17:10:50 +0300

    hibernate annotation

--- src/main/java/com/hibernatedemo/Main.java
+++ src/main/java/com/hibernatedemo/Main.java
@@ -1,9 +1,11 @@
 package com.hibernatedemo;
 
-public class App 
+
+public class Main 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	
+    	
     }
 }
