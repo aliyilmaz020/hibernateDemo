c3747d320312dcd1b2ece816c77c165ac590f7b8
aliyilmaz020 <aliyilmaz@ogr.bandirma.edu.tr>
2024-01-10 23:21:46 +0300

    hibernate annotation query

--- src/main/java/com/hibernatedemo/Main.java
+++ src/main/java/com/hibernatedemo/Main.java
@@ -1,11 +1,27 @@
 package com.hibernatedemo;
 
 
-public class Main 
-{
-    public static void main( String[] args )
-    {
-    	
-    	
-    }
+import java.util.List;
+
+import jakarta.persistence.*;
+
+public class Main {
+	public static void main(String[] args) {
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("MyPersistence");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+	    try {
+	    	entityManager.getTransaction().begin();
+
+	        List<City> cities = entityManager.createQuery("from City", City.class).getResultList();
+
+	        for (City city : cities) {
+	            System.out.println(city.getName() + ": " + city.getPopulation());
+	        }
+	        entityManager.getTransaction().commit();
+	    }finally {
+	    	entityManager.close();
+	    	entityManagerFactory.close();
+	    }
+	}
+
 }
